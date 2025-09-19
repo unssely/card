293a5d7cff5406a06255ba10870b9ac91d5d7f4e
unssely <susarinaulia91@gmail.com>
2025-09-19 12:18:36 +0500

    сщздание проекта

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const card = document.querySelector('.card'); //выбираем все элементы с классом card на странице 
+
+    card.addEventListener("click", ()=>{
+      console.log(0)
+        card.classList.toggle("card-show");
+    });
+
+
+
